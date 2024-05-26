2a3e41ba3d013d0d064e31e659454b4b67972c57
ammarsharif <ammarsharif377@gmail.com>
2024-05-26 17:43:34 +0500

    style sidebar button and cross icon

--- src/components/common/Sidebar.js
+++ src/components/common/Sidebar.js
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from 'react-redux';
-
+import { FaTimes } from 'react-icons/fa';
 import {
   addRecipeToPlanner,
   toggleSidebar,
@@ -24,8 +24,11 @@ const Sidebar = () => {
 
   return (
     <div className="fixed bottom-0 right-0  bg-gray-100 w-1/3 p-4 shadow-lg overflow-y-auto max-h-full">
-      <button className="mb-4" onClick={() => dispatch(toggleSidebar({}))}>
-        Close
+      <button
+        className="mb-4 p-2 bg-transparent text-red-500 border border-red-500 rounded-full hover:bg-red-500 hover:text-white transition duration-300 flex items-center justify-center"
+        onClick={() => dispatch(toggleSidebar({}))}
+      >
+        <FaTimes className="w-5 h-5" />
       </button>
       <h2 className="text-xl font-bold mb-4">Recipes</h2>
       <div className="space-y-4">
@@ -39,7 +42,7 @@ const Sidebar = () => {
             <h3 className="mt-2 font-bold">{recipe.title}</h3>
             <p>{recipe.duration}</p>
             <button
-              className="bg-blue-500 text-white p-2 mt-2"
+              className="bg-transparent text-blue-500 border border-blue-500 rounded-lg p-3 mt-2 hover:bg-blue-500 hover:text-white transition duration-300"
               onClick={() => handleAddToPlanner(recipe)}
             >
               Add to Planner
